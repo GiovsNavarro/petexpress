8f2d05466e11f7016d73245a215223386d3a1b2f
GiovsNavarro <137450592+GiovsNavarro@users.noreply.github.com>
2026-02-11 19:22:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function fazerPedido() {
+    const nome = document.getElementById("nome").value;
+    const pet = document.getElementById("pet").value;
+    const racao = document.getElementById("racao").value;
 
+    if (nome === "" || pet === "" || racao === "") {
+        document.getElementById("mensagem").innerText = "⚠️ Preencha todos os campos!";
+        return;
+    }
+
+    document.getElementById("mensagem").innerText =
+        `Pedido enviado com sucesso, ${nome}! A ração ${racao} para ${pet} chegará em breve 🐾`;
+}
